1dd0fffdb9a26f134cee23d897f3dc6729b2e1d3
atakriti <anwart256@gmail.com>
2022-06-27 13:43:34 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,7 @@
 let container = document.getElementById("container");
 
-
-
-async function randomMeal(e) {
+let item = 1;
+async function randomMeal() {
   let fetching = await fetch(
     `https://www.themealdb.com/api/json/v1/1/random.php`
   );
@@ -12,16 +11,31 @@ async function randomMeal(e) {
     <h2><span>Category: </span>${json.meals.map((p) => p.strCategory)} </h2>
     <h3><span>Country: </span>${json.meals.map((p) => p.strArea)} </h3>
     <img src= ${json.meals.map((p) => p.strMealThumb)}>
-    <strong><span>Ingredient: </span>${json.meals.map((p) => p.strIngredient1)}  ${json.meals.map((p) => p.strMeasure1)} </strong>
-    <p><span>Instructions: </span>${json.meals.map((p) => p.strInstructions)} </p>
-    <video controls ><source src =" https://www.youtube.com/watch?v=H5SmjR-fxUs" ></source></video>
-    
-    <iframe src = "https://www.youtube.com/watch?v=H5SmjR-fxUs" ></iframe>
 
-  
+    
+    <ul><span>Ingredient: </span>${json.meals.strIngredient1}   </ul>
+
+
+
+    <p><span>Instructions: </span>${json.meals.map((p) => p.strInstructions)} </p>
+
+    
+    
+    
     </div>`;
-  container.innerHTML += div;
-  return json;
-}
-randomMeal();
-// ${json.meals.map(p => p.strYoutube)}
+    container.innerHTML += div;
+    return json;
+  }
+  // randomMeal();
+  // ${json.meals.map(p => p.strYoutube)}
+  randomMeal();
+  // ${json.meals.map((p) => p.strMeasure1)}
+  
+  // for (let i = 0; i < 20; i++) {
+    //   i++;
+    // }
+
+
+    // <video controls ><source src =" https://www.youtube.com/watch?v=H5SmjR-fxUs" ></source></video>
+    
+    // <iframe src = "https://www.youtube.com/watch?v=H5SmjR-fxUs" ></iframe>
